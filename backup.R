2eeb3e8bb56c8155d325a575f5a3ebd4628dc802
Frank Xu <franklolxu@gmail.com>
2019-04-02 22:40:46 -0400

    Logistic Regression Done

---
+++ backup.R
@@ -0,0 +1,9 @@
+train$MMRAcquisitionAuctionAveragePrice = as.numeric(train$MMRAcquisitionAuctionAveragePrice)
+train$MMRAcquisitionAuctionCleanPrice = as.numeric(train$MMRAcquisitionAuctionCleanPrice)
+train$MMRAcquisitionRetailAveragePrice = as.numeric(train$MMRAcquisitionRetailAveragePrice)
+train$MMRAcquisitonRetailCleanPrice = as.numeric(train$MMRAcquisitonRetailCleanPrice)
+
+train$MMRCurrentAuctionAveragePrice = as.numeric(train$MMRCurrentAuctionAveragePrice)
+train$MMRCurrentAuctionCleanPrice = as.numeric(train$MMRCurrentAuctionCleanPrice)
+train$MMRCurrentRetailAveragePrice = as.numeric(train$MMRCurrentRetailAveragePrice)
+train$MMRCurrentRetailCleanPrice = as.numeric(train$MMRCurrentRetailCleanPrice)
